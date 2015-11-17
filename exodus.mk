bc1eb65feddfd9c99586f9fb530578e3914fe8e4
MartinRo <martin@rodemann.org>
2015-11-17 12:06:28 +0000

    Bringup for Exodus

--- exodus.mk
+++ exodus.mk
@@ -2,11 +2,11 @@
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Exodus stuff.
+$(call inherit-product, vendor/exodus/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/exodus/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
@@ -14,7 +14,7 @@ $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
 DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay-cm
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_shamu
+PRODUCT_NAME := exodus_shamu
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6
 

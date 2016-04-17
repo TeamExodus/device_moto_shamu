a3a90a18b913e26c2efaeb6ee6975a9741e998b3
Martin Rodemann <martin@rodemann.org>
2016-04-17 09:12:10 +0000

    Drowning eye for shamu
    Change-Id: I38592c6ee733d5124b1a675e76f4fc91408a691e

--- device.mk
+++ device.mk
@@ -53,6 +53,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
+# Exodus BSP
+TARGET_USES_EXODUS_BSP := true
+
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
